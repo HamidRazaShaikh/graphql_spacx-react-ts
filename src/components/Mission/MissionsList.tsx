2a72eb0f9dce400a299a400752eda2f4df5d5632
HamidRazaVision <hamidraza.chemistry@gmail.com>
2020-11-19 20:51:25 -0800

    drop down list added

--- src/components/Mission/MissionsList.tsx
+++ src/components/Mission/MissionsList.tsx
@@ -1,37 +1,48 @@
 import React from "react";
 import { LaunchesInfoQuery } from "./../../generated/graphql";
-import ListGroup from 'react-bootstrap/listGroup';
+import ListGroup from "react-bootstrap/listGroup";
 import Container from "react-bootstrap/Container";
-import './index.css';
+import InputGroup from "react-bootstrap/InputGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
-
+import "./index.css";
 
 export interface idProps {
-
-  handlePassId : ( newId : number)=> void
+  handlePassId: (newId: number) => void;
 }
-
 
 interface Props extends idProps {
-  data: LaunchesInfoQuery
- 
+  data: LaunchesInfoQuery;
 }
 
-const MissionList: React.FC<Props> = ({ data , handlePassId}) => {
+const MissionList: React.FC<Props> = ({ data, handlePassId }) => {
   return (
     <Container>
-      <ListGroup className = 'list'>
-        {data.launches?.map((launchObj, id) => {
+      <InputGroup className="mb-3">
+        <DropdownButton
+          as={InputGroup.Prepend}
+          variant="outline-secondary"
+          title="Dropdown"
+          id="input-group-dropdown-1"
+        >
+          {data.launches?.map((launchObj, id) => {
+            let key = id + 1;
 
-          let key = id + 1
-      
-          return <ListGroup.Item className = 'listItem' key = {id} onClick = { ()=> handlePassId(key)}>{launchObj?.mission_name}</ListGroup.Item>;
-        })}
-      </ListGroup>
+            return (
+              <Dropdown.Item
+                className="listItem"
+                key={id}
+                onClick={() => handlePassId(key)}
+              >
+                {launchObj?.mission_name}
+              </Dropdown.Item>
+            );
+          })}
+        </DropdownButton>
+      </InputGroup>
     </Container>
   );
 };
 
 export default MissionList;
-
-
