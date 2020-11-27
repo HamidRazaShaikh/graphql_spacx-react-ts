37ed9205d0295b1d8a79597b8f8822f118cf08a7
HamidRazaVision <hamidraza.chemistry@gmail.com>
2020-11-27 05:37:57 -0800

    deoployed on surge throug CI

---
+++ build/precache-manifest.f3ca20462e7467a4419d775c8bd36162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50237f544a72157ca4f98350c09283f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44af830b4778fc90f556",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b1167e11a9f93e87cc51",
+    "url": "/static/css/main.5ad9fce4.chunk.css"
+  },
+  {
+    "revision": "44af830b4778fc90f556",
+    "url": "/static/js/2.3300e545.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.3300e545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1167e11a9f93e87cc51",
+    "url": "/static/js/main.14600c89.chunk.js"
+  },
+  {
+    "revision": "a2234a7ace356afc8a04",
+    "url": "/static/js/runtime-main.139f9db2.js"
+  }
+]);
